ab8a3b0a3e9beead108751fb48e56aaaf429b474
karinaromano <karinadevuk@gmail.com>
2024-08-05 17:35:34 -0300

    updates

--- day2-data-types.py
+++ day2-data-types.py
@@ -41,6 +41,43 @@ print(False)
 #Booleans don't have quotation marks around them,
 # otherwise it would turn them into a String. So it should be bool = False
 
+# print(len(123456)) #TypeError: object of type 'int' has no len()
+print(len("Hello"))
 
+#How would we know what is the Data Type fo "Hello"?
+#Function => type("Hello") => for type checking
+type("Hello")
+print(type("Hello")) # <class 'str'>
+print(type(123456)) # <class 'int'>
 
+#Practice
+# using type and print functions to print out 4 lines into
+# the output area so we get the full collection of data type
+print(type("Karina")) #<class 'str'>
+print(type(8061979)) #<class 'int'>
+print(type(123.918)) #<class 'float'>
+print(type(True)) #<class 'bool'> True or False with Capital T or F
+
+#Type Conversion also knowing as Type Casting
+"123" #this is a string but I want to convert to a number:
+int("123")
+print(int("123")) #123
+print(int("123") + int("456")) #579
+
+int()
+float()
+str()
+bool()
+
+#Practice
+#make this line of code run without errors
+# print("Number of letters in your name: " + len(input("Enter your name")))
+
+name_of_the_user = input("Enter your name")
+lenght_of_name = len(name_of_the_user)
+
+#print(type("Number of letters in your name: ")) #str
+#print(type(lenght_of_name)) #int
+
+print("Number of letters in your name: " + str(lenght_of_name))
 
