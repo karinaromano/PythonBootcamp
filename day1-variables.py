346fc650d360159f62c799924f503c16a5db1c79
karinaromano <karinadevuk@gmail.com>
2024-08-04 21:16:37 -0300

    Day1
    Day 1 of 100 Days of Code: Python Bootcamp

---
+++ day1-variables.py
@@ -0,0 +1,10 @@
+name = "Jack"
+print(name)
+
+name = input("What is your name?")
+length = len(name)
+print(length)
+
+#variables names:
+#never use spaces # give meaninful names # use undescore
+#for double names as user_name # you can use number but by the end of the variable
---
+++ day1.py
@@ -0,0 +1,57 @@
+# print() what text to be printed
+# input() function
+# input("A prompt for the user")
+#https://thonny.org/
+
+print("Hello World!")
+print('She said: "Hello" and then left.')
+print("She said: \"Hello\" and then left.")
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("Hello World!\nHello World!\nHello World!")
+
+print("Hello " + "Karina")
+print("Hello" + " Karina")
+print("Hello" +" "+ "Karina")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+input("What is your name?")
+
+print("Hello " + input("What is your name?"))
+
+#print(input("What is your name?")) #to avoid
+print(input())
+
+num1 = int(input())
+num2 = int(input())
+print(num1 * num2)
+
+len(input()) #to know howw many letters the input have
+print(len(input()))
+
+numOfLetters = len("Karina")
+print(numOfLetters)
+
+#Write a program that calculates and outputs the number of characters
+#in any name. The automated tests will try out lots of different
+#names as the input. Your code should work for any name. Your code
+#should only output the number, no other text is needed.
+print(len(input()))
+
+input() #can bring the value of the input pane
+len(input()) #length function, how many letters in the input pane
+print(len(input())) #
+
+#Python Variables
+# input("What is your name?")
+name = input("What is your name?")
+print(name)
+
+
