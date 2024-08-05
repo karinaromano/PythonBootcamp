361e090f0c97feb6a56c9dec89f13234bc045722
karinaromano <karinadevuk@gmail.com>
2024-08-05 15:00:48 -0300

    Day2-DataTypes

---
+++ day2-data-types.py
@@ -0,0 +1,46 @@
+# Data Types, Numbers, Operations, Type Conversion and f-Strings
+# Build a tip calculator
+
+#lenght function to know how many characters the string has
+print(len("Hello"))
+
+#Data Types
+#String
+#Integers
+#Floats
+#Booleans
+
+#Subscripting
+print("Hello"[0]) # programmers always start counting from zero and also counts the space
+#This method of pulling out a particular element from a string is called #subscripting
+
+
+print("Hello"[4]) #
+print("hello"[-1]) #negative indices -1 last character
+print("Hello"[-2]) # from the end to the beginning <-
+
+#123 is treated as a number
+#"123" is treated as any other piece of text
+
+#String
+print("123" + "345") # Concatenation
+
+#Integer = whole number -. write the number without anything else
+print(123 + 345)
+
+#Large Integers -> for large numbers we use to put commas
+print(123_456_789) #in python we use Underscore _ between large numbers more readable,
+#but the computer will treat as a whole large number and ignores the underscore
+
+#Float = Floating point number (Decimal places)
+print(3.14159)
+
+#Boolean = True or False
+print(True)
+print(False)
+#Booleans don't have quotation marks around them,
+# otherwise it would turn them into a String. So it should be bool = False
+
+
+
+
